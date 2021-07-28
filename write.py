4c58e2b5638885393dfcc55ce4fab12d2b98715d
Edison Garcia <egachi@live.com>
2021-07-27 20:44:23 -0500

    Update write.py

--- write.py
+++ write.py
@@ -1,15 +1,12 @@
-from __future__ import print_function
-import lldb
-import os
 import lldb
 
 def __lldb_init_module (debugger, dict):
   debugger.HandleCommand('command script add -f write.write_to_file write')
 
 def write_to_file(debugger, filename, command, result, dict):
-  f=open(filename, 'w')
+  f=open(filename,'w')
   debugger.SetOutputFileHandle(f,True);
-  debugger.HandleCommand(command)
-  f.close();
-  debugger.SetOutputFileHandle(sys.stdout, True)
+  debugger.HandleCommand(command)  
+  f.close()
+  debugger.SetOutputFileHandle(sys.__stdout__, True)
 
