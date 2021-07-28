7458bd7527021fd49642f6b6ff4c904d08a00753
Edison Garcia <egachi@live.com>
2021-07-27 20:40:44 -0500

    Update write.py

--- write.py
+++ write.py
@@ -6,7 +6,7 @@ import lldb
 def __lldb_init_module (debugger, dict):
   debugger.HandleCommand('command script add -f write.write_to_file write')
 
-def write_to_file(debugger,command, result, dict):
+def write_to_file(debugger, filename, command, result, dict):
   f=open(filename, 'w')
   debugger.SetOutputFileHandle(f,True);
   debugger.HandleCommand(command)
