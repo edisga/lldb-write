9f911309a3eeb0ce651c15a87a0910168cce65c8
Edison Garcia <egachi@live.com>
2021-07-27 20:23:21 -0500

    Update write.py

--- write.py
+++ write.py
@@ -44,7 +44,6 @@ def write_to_file(filename, command, output):
     f.write("(lldb) " + command + '\n\n')
     f.write(str(output))
     #f.write(strip_esc_seq(output))
-    f.flush();
     f.close();
 
 
